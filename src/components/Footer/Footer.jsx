75d3cfbb4a401145792ce2fe67037af5bba54a5a
CugliariSanti17 <cugliarisantiago@gmail.com>
2025-12-03 00:21:26 -0300

    agrego responsive del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,16 @@
 
 const Footer = () => {
   return (
-    <footer className="bg-[#1F1D2B] text-gray-400 text-center py-6 border-t border-[#262837] mt-10 ml-28">
+    <footer className="bg-[#1F1D2B] text-gray-400 text-center py-6 border-t border-[#262837] mt-10 px-4 pb-24 md:pb-6 md:ml-28">
       <p className="text-sm">
-        © {new Date().getFullYear()} <span className="text-[#ec7c6a] font-semibold">Logo</span> — Todos los derechos reservados.
+        © {new Date().getFullYear()}{" "}
+        <span className="text-[#ec7c6a] font-semibold">Logo</span> — Todos los
+        derechos reservados.
       </p>
+
       <p className="text-xs mt-2">
-        Desarrollado por <span className="text-[#ec7c6a] font-medium">Santiago Cugliari</span>
+        Desarrollado por{" "}
+        <span className="text-[#ec7c6a] font-medium">Santiago Cugliari</span>
       </p>
     </footer>
   )
